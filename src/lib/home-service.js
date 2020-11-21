7a4904b50468442d1eb74650f76487853f92a578
Eric Blair <erbmart7@gmail.com>
2020-11-21 11:14:54 +0100

    here we go

--- src/lib/home-service.js
+++ src/lib/home-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Home {
   constructor() {
     this.home = axios.create({
-      baseURL: "http://localhost:4000",
+      baseURL: process.env.REACT_APP_API_URI,
       withCredentials: true,
     });
   }
--- src/lib/profile-service.js
+++ src/lib/profile-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Profile {
   constructor() {
     this.profile = axios.create({
-      baseURL: "http://localhost:4000",
+      baseURL: process.env.REACT_APP_API_URI,
       withCredentials: true,
     });
   }
